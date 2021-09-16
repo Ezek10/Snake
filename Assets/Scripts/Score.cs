ab63cf7121178dc9a3ed7d7552f70b6e421991f3
Ezequiel  Marcel <Ezequielmarcel2@gmail.com>
2021-09-15 23:23:34 -0300

    v5

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Score : MonoBehaviour
 {
     // Start is called before the first frame update
     private int score = 0;
-    private int maxScore = 0;
+    private int maxScore;
     public Text texto;
+    private int record;
     void Start()
     {
         texto.text = "Score: " + score.ToString() + "\nMaxScore: " + maxScore.ToString();
+        maxScore = PlayerPrefs.GetInt("record",-3);
     }
 
     // Update is called once per frame
@@ -19,7 +22,10 @@ public class Score : MonoBehaviour
     {
         score += s;
         if (score > maxScore)
+        {
             maxScore = score;
+            PlayerPrefs.SetInt("record",maxScore);
+        }
         texto.text = "Score: " + score.ToString() + "\nMaxScore: " + maxScore.ToString();
     }
     public void DeleteScore()
@@ -27,4 +33,5 @@ public class Score : MonoBehaviour
         score = 0;
         texto.text = "Score: " + score.ToString() + "\nMaxScore: " + maxScore.ToString();
     }
+
 }
